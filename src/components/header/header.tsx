e407a60d5b7716a4ddd1fbe7f85292f3732a9ae1
Md-Hazrat <mdhazratali8866@gmail.com>
2024-09-06 01:13:28 +0600

    working on navbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,46 @@
 "use client";
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+import Image from "next/image";
+import { Col, Row } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 
 const Header = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary container">
-      <Container>
-        <Navbar.Brand href="#home">FurniFlex</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Categories</Nav.Link>
-            <Nav.Link href="#link">Custom</Nav.Link>
-            <Nav.Link href="#link">Blogs</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="container p-4">
+      <Navbar>
+        <Row className="w-100">
+          <Col
+            className="d-flex gap-2 align-items-center justify-content-start"
+            md={3}
+          >
+            <Image
+              className="cursor-pointer me-2"
+              alt="logo"
+              src="/images/logo.png"
+              width={134}
+              height={38}
+            />
+          </Col>
+
+          <Col className="d-flex justify-content-center gap-4" md={6}>
+            <p className="cursor-pointer mb-0">Home</p>
+            <p className="cursor-pointer mb-0">Products</p>
+            <p className="cursor-pointer mb-0">Categories</p>
+            <p className="cursor-pointer mb-0">Custom</p>
+            <p className="cursor-pointer mb-0">Blog</p>
+          </Col>
+
+          <Col
+            className="d-flex justify-content-end gap-4 align-items-center"
+            md={3}
+          >
+            <p className="cursor-pointer mb-0">Cart</p>
+            <p className="cursor-pointer mb-0">Login</p>
+          </Col>
+        </Row>
+        <hr className="my-4" style={{ color: "red" }} />
+      </Navbar>
+    </div>
   );
 };
 
